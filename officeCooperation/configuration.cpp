05fbbcc36e44b103ccd8bb71dfc8ded559a7d1a8
ddlwarrior <67677478+ddlwarriors@users.noreply.github.com>
2022-06-29 15:08:37 +0800

    Update configuration.cpp

--- officeCooperation/configuration.cpp
+++ officeCooperation/configuration.cpp
@@ -1,5 +1,5 @@
 #include"configuration.h"
-extern const QHostAddress SERVER_IP=QHostAddress("192.168.154.128");//服务器
+extern const QHostAddress SERVER_IP=QHostAddress("***");//服务器IP地址，就是运行Server的服务器地址
 extern const int CHAT_PORT = 25000;//聊天端口
 extern const int FILE_PORT = 26000;//调度文件传输端口
 extern int NUMBER_OF_BACKGROUNDS = 8;//壁纸数量
