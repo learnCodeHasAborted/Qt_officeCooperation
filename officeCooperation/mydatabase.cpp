e90a126dd2533cac56374bf426ef8a8dee4e131b
ddlwarrior <67677478+ddlwarriors@users.noreply.github.com>
2022-06-29 15:07:31 +0800

    Update mydatabase.cpp

--- officeCooperation/mydatabase.cpp
+++ officeCooperation/mydatabase.cpp
@@ -4,11 +4,10 @@ MyDatabase::MyDatabase(QString name)
 {
     db = QSqlDatabase::addDatabase("QMYSQL",name);
     // 数据库连接需要设置的信息
-    db.setHostName("49.235.84.252"); // 数据库服务器IP
-    db.setDatabaseName("officeCooperation");// 数据库名
-//    db.setDatabaseName("test");// 数据库名
-    db.setUserName("guest");// 用户名
-    db.setPassword("DOvBFEtrf9/d");// 密码
+    db.setHostName("***"); // 数据库服务器IP，把 *** 改成自己的数据库 IP 地址，下面也要改
+    db.setDatabaseName("***");// 数据库名
+    db.setUserName("***");// 用户名
+    db.setPassword("***");// 密码
     db.setPort(3306);// 端口号
     db.open();
 }
